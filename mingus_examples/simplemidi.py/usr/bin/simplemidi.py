ec9085d5712c8f355dc5e3c358118672a76e6f6d
Greg DeKoenigsberg <greg.dekoenigsberg@gmail.com>
2015-12-18 17:06:41 -0500

    Delete simplemidi.py
    Wrong location... grr

--- mingus_examples/simplemidi.py/usr/bin/simplemidi.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-from mingus.midi import midi_file_out
-from mingus.containers import NoteContainer
-
-nc = NoteContainer(["A", "C", "E"])
-midi_file_out.write_NoteContainer("test.mid", nc)
